fff3071e6139dd1cd0ee5bdb91707755e0e31133
flyinghighp <7006976@spsd.sk.ca>
2025-03-04 06:11:37 -0600

    Generated Random Rectangles which can be resized with the arrow keys

--- Terrain Generator Assignment 02/sketch.js
+++ Terrain Generator Assignment 02/sketch.js
@@ -1,15 +1,43 @@
-// Project Title
+// Terrain Generator Assignment 02
 // Priyansh Jhanji
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// 3rd March 2025
 
+let yLimit;
+let yHeight;
+let rectWidth;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  yLimit = random(100, 300);
+  yHeight = random(0, yLimit);
+  rectWidth = width / 20;
+  generateTerrain();
 }
 
 function draw() {
-  background(220);
+  
+}
+
+function generateTerrain() {
+  let numRects = 20;
+  let x = 0;
+
+  background("white");
+
+  while (x < width) {
+    let rectHeight = random(50, height);
+    fill("black");
+    stroke("white");
+    rect(x, height - rectHeight, rectWidth, rectHeight);
+    x += rectWidth;
+  }
+}
+
+function keyPressed() {
+  if (keyCode === LEFT_ARROW) {
+    rectWidth = max(10, rectWidth - 20);
+  } else if (keyCode === RIGHT_ARROW) {
+    rectWidth = min(40, rectWidth + 20);
+  }
+  generateTerrain();
 }
