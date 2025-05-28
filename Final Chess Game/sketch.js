2525218176508de48d1c49ed002474c0724da825
flyinghighp <7006976@spsd.sk.ca>
2025-05-28 12:16:48 -0600

    Started onto the restart button

--- Final Chess Game/sketch.js
+++ Final Chess Game/sketch.js
@@ -7,6 +7,7 @@ let gameState = 'menu';
 let startBtn;
 let puzzBtn;
 let ovoBtn;
+let restartBtn;
 let thickness = 30;
 let size;
 let cam;
@@ -28,6 +29,7 @@ let promotionPending = false;
 let promotionPiece = null;
 let promotionButtons = [];
 let promotionInProgress = false;
+let restartImg;
 
 let biB; let bk; let qb; let br; let bp; let bkni;
 let biW; let wk; let qw; let wr; let wp; let wkni;
@@ -55,7 +57,7 @@ function preload() {
   whiteWinImg = loadImage("assets/whiteWins.png");
   blackWinImg = loadImage("assets/blackWins.png");
   stalemateImg = loadImage("assets/stalemate.png");
-
+  restartImg = loadImage("assets/restart.png")
 }
 
 function setup() {
@@ -67,7 +69,7 @@ function setup() {
   cam.lookAt(0, 0, 0);
   angleMode(DEGREES);
   
-  startBtn = createButton('Play Vs Ai');
+  startBtn = createButton('Play Vs A.I');
   startBtn.addClass('start-ui');
   startBtn.mousePressed(() => {
     gameState = 'play';
@@ -75,6 +77,7 @@ function setup() {
     startBtn.hide();
     ovoBtn.hide();
     resignBtn.show();
+    restartBtn.show();
     document.getElementById("gifBackground").style.display = "none";
   });
 
@@ -89,21 +92,37 @@ function setup() {
     puzzBtn.hide();
     startBtn.hide();
     ovoBtn.hide();
+    restartBtn.show();
     document.getElementById("gifBackground").style.display = "none";
   });
 
   ovoBtn = createButton('1 V 1');
   ovoBtn.addClass('start-ui');
-  ovoBtn.position(width/2, height/2-100); 
+  ovoBtn.position(width/2, height/2-900); 
   ovoBtn.mousePressed(() => {
     gameState = 'ovo';
     puzzBtn.hide();
     startBtn.hide();
     resignBtn.show();
+    restartBtn.show();
     ovoBtn.hide();
     document.getElementById("gifBackground").style.display = "none";
   });
 
+  restartBtn = createButton('');
+  restartBtn.style('background-image', `url('assets/restart.png')`);
+  restartBtn.style('background-size', 'cover');
+  restartBtn.style('width', '50px');
+  restartBtn.style('height', '50px');
+  restartBtn.style('border', 'none');
+  restartBtn.style('position', 'fixed'); 
+  restartBtn.style('bottom', '20px');    
+  restartBtn.style('right', '20px');     
+  restartBtn.mousePressed(() => {
+    gameState = 'menu'; 
+  });
+
+  
   size = min(width, height) / 10;
   chessBoard = new ChessBoard();
   chessBoard.createBoard(0, 0);
@@ -241,7 +260,7 @@ function draw() {
 
   if (!gameOver && gameState === 'ovo') {
     updateScoreDisplay();
-
+    
     resignBtn.hide();
 
     const opp = currentTurn === 'white' ? 'black' : 'white';
